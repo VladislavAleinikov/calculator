57c8c6e274905015f0737fcf628a66afff073aa1
Vlad Aleynikov <vladislav.aleinikov@innowise.com>
2025-07-19 20:14:25 +0300

    move index.js to script folder

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   "mode": "none",
-  "entry": "./src/index.js",
+  "entry": "./src/script/index.js",
   "output": {
     "path": path.resolve(__dirname + "/dist"),
     "filename": "main.js",
